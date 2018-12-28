5785446de96fe5ed3eb4f0929456991b6cffece1
DENIS PLASTER <denjeo@gmail.com>
2018-12-28 13:03:45 -0600

    added condidtional statements for show info button

--- client/src/components/Book.js
+++ client/src/components/Book.js
@@ -4,13 +4,32 @@ export default class Book extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      count: 0,
+      showInfo: true
     };
   }
+
+  handleInfo = () => {
+    this.setState({
+      showInfo: !this.state.showInfo
+    });
+  };
 
   render() {
     const { img, title, author } = this.props.info;
     const { deleteItem } = this.props;
+    const checkInfo = info => {
+      if (info === true) {
+        return (
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt,
+            quis?
+          </p>
+        );
+      } else {
+        return null;
+      }
+    };
 
     return (
       <div className="book">
@@ -19,6 +38,19 @@ export default class Book extends Component {
           <h4>Title : {title}</h4>
           <h6>By : {author}</h6>
           <button onClick={deleteItem}> delete item</button>
+          <button onClick={this.handleInfo}>show info</button>
+          {checkInfo(this.state.showInfo)}
+          {/* {this.state.showInfo && (
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Distinctio, ratione?
+            </p>
+          )} */}
+          {/* {this.state.showInfo ? (
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, at?
+            </p>
+          ) : null} */}
         </div>
       </div>
     );
