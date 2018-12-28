cd2bfbdbaf6011f7bf1a4f1fc41ede28a4fa4c05
DENIS PLASTER <denjeo@gmail.com>
2018-12-27 23:48:40 -0600

    Updates

---
+++ precache-manifest.020548df87a89d02514149c06e92fe53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4de98774e88cebcee4f6",
+    "url": "/react-gh-pages/static/css/main.40566575.chunk.css"
+  },
+  {
+    "revision": "4de98774e88cebcee4f6",
+    "url": "/react-gh-pages/static/js/main.4de98774.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/react-gh-pages/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "4ccf2466ebf6dd95ea424c318e305ccb",
+    "url": "/react-gh-pages/index.html"
+  }
+];
